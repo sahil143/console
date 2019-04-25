20a1bd56f9e77a5b1ed65f0624cb6218fd0760d2
christianvogt <cvogt@redhat.com>
2019-04-25 12:22:32 -0400

    add selection capabilities to topology nodes

--- frontend/public/extend/devconsole/components/topology/Node/BaseNode.tsx
+++ frontend/public/extend/devconsole/components/topology/Node/BaseNode.tsx
@@ -21,47 +21,58 @@ const BaseNode: React.FunctionComponent<BaseNodeProps> = ({
   selected,
   icon,
   label,
+  onSelect,
 }: BaseNodeProps) => (
-    <g transform={`translate(${x}, ${y})`}>
-      <defs>
-        <pattern
-          id="image"
+  <g
+    transform={`translate(${x}, ${y})`}
+    onClick={
+      onSelect
+        ? (e) => {
+          e.stopPropagation();
+          onSelect();
+        }
+        : null
+    }
+  >
+    <defs>
+      <pattern
+        id="image"
+        x="0"
+        y="0"
+        height="100%"
+        width="100%"
+        viewBox={`0 0 ${innerRadius} ${innerRadius}`}
+      >
+        <image
           x="0"
           y="0"
-          height="100%"
-          width="100%"
-          viewBox={`0 0 ${innerRadius} ${innerRadius}`}
-        >
-          <image
-            x="0"
-            y="0"
-            width={innerRadius}
-            height={innerRadius}
-            xlinkHref={icon ? `/static/assets/${icon}.svg` : '/static/assets/openshift.svg'}
-          />
-        </pattern>
-      </defs>
-      <circle cx={0} cy={0} r={outerRadius} fill="#fff" />
-      <circle cx={0} cy={0} r={innerRadius} fill="url(#image)" />
-      <text
-        textAnchor="middle"
-        style={{ fontSize: outerRadius * 0.25 }}
-        y={outerRadius + outerRadius * 0.25}
-        x={0}
-      >
-        {label ? label : 'DeploymentConfig'}
-      </text>
-      {selected && (
-        <circle
-          cx={0}
-          cy={0}
-          r={outerRadius + outerRadius * 0.03}
-          fill="transparent"
-          stroke="#77BAFF"
-          strokeWidth={outerRadius * 0.06}
+          width={innerRadius}
+          height={innerRadius}
+          xlinkHref={icon ? `/static/assets/${icon}.svg` : '/static/assets/openshift.svg'}
         />
-      )}
-    </g>
-  );
+      </pattern>
+    </defs>
+    <circle cx={0} cy={0} r={outerRadius} fill="#fff" />
+    <circle cx={0} cy={0} r={innerRadius} fill="url(#image)" />
+    <text
+      textAnchor="middle"
+      style={{ fontSize: outerRadius * 0.25 }}
+      y={outerRadius + outerRadius * 0.25}
+      x={0}
+    >
+      {label ? label : 'DeploymentConfig'}
+    </text>
+    {selected && (
+      <circle
+        cx={0}
+        cy={0}
+        r={outerRadius + outerRadius * 0.03}
+        fill="transparent"
+        stroke="#77BAFF"
+        strokeWidth={outerRadius * 0.06}
+      />
+    )}
+  </g>
+);
 
 export default BaseNode;
