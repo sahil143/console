88f3e60fbd406e3f002f8a644793bd2ab53b42c7
Sahil Budhwar <sahilbudhwar143@gmail.com>
2021-03-01 16:08:12 +0530

    create infra to execute commands in terminal .. fix types for cloud shell command action

--- frontend/packages/console-app/src/components/quick-starts/QuickStartMarkdownView.tsx
+++ frontend/packages/console-app/src/components/quick-starts/QuickStartMarkdownView.tsx
@@ -3,6 +3,7 @@ import { extension } from 'showdown';
 import { SyncMarkdownView } from '@console/internal/components/markdown-view';
 import { MarkdownHighlightExtension } from '@console/shared';
 import { HIGHLIGHT_REGEXP } from '@console/shared/src/components/markdown-highlight-extension/highlight-consts';
+import { useCloudShellCommandDispatch } from '../../redux/actions/cloud-shell-actions';
 
 const EXTENSION_NAME = 'quickstart';
 extension(EXTENSION_NAME, () => {
@@ -15,12 +16,42 @@ extension(EXTENSION_NAME, () => {
         return `<button class="pf-c-button pf-m-inline pf-m-link" data-highlight="${linkId}">${linkLabel}</button>`;
       },
     },
+    {
+      type: 'lang',
+      regex: /`(.*?)`{{execute}}/g,
+      replace: (text: string, linkLabel: string, linkType: string, linkId: string): string => {
+        if (!linkLabel || !linkType || !linkId) return text;
+        return `<code id="${linkType}" data-execute-text="${linkType}">${linkLabel}</code><button data-execute-for="${linkType}">Execute</button>`;
+      },
+    },
   ];
 });
 
 type QuickStartMarkdownViewProps = {
   content: string;
   exactHeight?: boolean;
+};
+
+const QuickStartMarkdownExecute = ({ docContext, rootSelector }) => {
+  const setCloudShellCommand = useCloudShellCommandDispatch();
+  React.useEffect(() => {
+    const elements = docContext.querySelectorAll(`${rootSelector} [data-execute-for]`);
+    const execute = (e) => {
+      const attributeValue = e.target.getAttribute('data-execute-for');
+      const textToExecute = docContext.querySelector(`[data-execute-text="${attributeValue}"]`)
+        .innerText;
+      // eslint-disable-next-line
+      console.log('Excuted:', textToExecute);
+      setCloudShellCommand(textToExecute);
+    };
+
+    elements && elements.forEach((elm) => elm.addEventListener('click', execute));
+    return () => {
+      elements && elements.forEach((elm) => elm.removeEventListener('click', execute));
+    };
+  }, [docContext, rootSelector, setCloudShellCommand]);
+
+  return null;
 };
 
 const QuickStartMarkdownView: React.FC<QuickStartMarkdownViewProps> = ({
@@ -34,11 +65,14 @@ const QuickStartMarkdownView: React.FC<QuickStartMarkdownViewProps> = ({
       exactHeight={exactHeight}
       extensions={[EXTENSION_NAME]}
       renderExtension={(docContext, rootSelector) => (
-        <MarkdownHighlightExtension
-          key={content}
-          docContext={docContext}
-          rootSelector={rootSelector}
-        />
+        <>
+          <MarkdownHighlightExtension
+            key={content}
+            docContext={docContext}
+            rootSelector={rootSelector}
+          />
+          <QuickStartMarkdownExecute docContext={docContext} rootSelector={rootSelector} />
+        </>
       )}
     />
   );
--- frontend/packages/console-app/src/redux/actions/cloud-shell-actions.ts
+++ frontend/packages/console-app/src/redux/actions/cloud-shell-actions.ts
@@ -1,12 +1,30 @@
+import { useCallback } from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore: FIXME missing exports due to out-of-sync @types/react-redux version
+import { useDispatch, Dispatch } from 'react-redux';
 import { RootState } from '@console/internal/redux';
-import { Dispatch } from 'react-redux';
 import { action, ActionType } from 'typesafe-actions';
 import { isCloudShellExpanded, isCloudShellActive } from '../reducers/cloud-shell-selectors';
 
 export enum Actions {
   SetCloudShellExpanded = 'setCloudShellExpanded',
   SetCloudShellActive = 'setCloudShellActive',
+  SetCloudShellCommand = 'SetCloudShellCommand',
 }
+
+const cloudShellCommand = (command: string | null) =>
+  action(Actions.SetCloudShellCommand, { command });
+
+export const useCloudShellCommandDispatch = (): ((command: string | null) => void) => {
+  const dispatch = useDispatch();
+  const setCloudShellCommand = useCallback(
+    (command: string) => {
+      dispatch(cloudShellCommand(command));
+    },
+    [dispatch],
+  );
+  return setCloudShellCommand;
+};
 
 export const setCloudShellExpanded = (isExpanded: boolean) =>
   action(Actions.SetCloudShellExpanded, { isExpanded });
@@ -31,6 +49,7 @@ export const setCloudShellActive = (isActive: boolean) =>
 const actions = {
   setCloudShellExpanded,
   setCloudShellActive,
+  cloudShellCommand,
 };
 
 export type CloudShellActions = ActionType<typeof actions>;
